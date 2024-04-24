ea981ab907514af0c45bfbe1e0d84cd488c55468
matheus1714 <matheussilvamartins1714@gmail.com>
2024-04-24 20:55:21 -0300

    config(web): configure next, tailwind and shadcn-ui

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,5 +1,9 @@
-export default function Page() {
+import { Button } from "../components/ui/button"
+
+export default function Home() {
   return (
-    <h1>Home</h1>
+    <div>
+      <Button>Click me</Button>
+    </div>
   );
 }
