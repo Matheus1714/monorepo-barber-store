38bd748e7d69ec41a56874d2f3aeb5ac5cdec5bc
matheus1714 <matheussilvamartins1714@gmail.com>
2024-04-28 11:14:33 -0300

    feat(web): add validation forgot password

--- apps/web/app/[locale]/auth/forgot-password/page.tsx
+++ apps/web/app/[locale]/auth/forgot-password/page.tsx
@@ -1,3 +1,11 @@
+'use client'
+
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { Form, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form';
+import { z } from "zod"
+import { useToast } from "@/components/ui/use-toast"
+
 import Link from "next/link"
 
 import { Button } from "@/components/ui/button"
@@ -5,7 +13,28 @@ import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 import { AuthLayout } from "@/components/auth-layout"
 
+const FormSchema = z.object({
+  email: z.string({
+    required_error: "É necessário colocar o email",
+  }).min(2, {
+    message: "Email deve conter pelo menos duas letras"
+  }).email("Email no formato inválido"),
+})
+
 export default function ForgotPasswordPage() {
+  const { toast } = useToast()
+
+  const form = useForm<z.infer<typeof FormSchema>>({
+    resolver: zodResolver(FormSchema),
+  })
+
+  function onSubmit(data: z.infer<typeof FormSchema>) {
+    toast({
+      title: "Email enviado para resetar a senha",
+    })
+  }
+
+
   return (
     <AuthLayout>
       <div className="grid gap-2 text-center">
@@ -14,20 +43,33 @@ export default function ForgotPasswordPage() {
           Digite seu email para recuperar a senha
         </p>
       </div>
-      <div className="grid gap-4">
-        <div className="grid gap-2">
-          <Label htmlFor="email">Email</Label>
-          <Input
-            id="email"
-            type="email"
-            placeholder="m@example.com"
-            required
-          />
-        </div>
-        <Button type="submit" className="w-full">
-          Recuperar Senha
-        </Button>
-      </div>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+          <div className="grid gap-4">
+            <div className="grid gap-2">
+              <FormField
+                control={form.control}
+                name="email"
+                render={({field}) => (
+                  <FormItem>
+                    <FormLabel>Email</FormLabel>
+                    <Input
+                      id="email"
+                      type="email"
+                      placeholder="m@example.com"
+                      {...field}
+                    />
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>
+            <Button type="submit" className="w-full">
+              Recuperar Senha
+            </Button>
+          </div>
+        </form>
+      </Form>
       <div className="mt-4 text-center text-sm">
         Não tem uma conta?{" "}
         <Link href="/pt/auth/register" className="underline">
@@ -35,7 +77,7 @@ export default function ForgotPasswordPage() {
         </Link>
       </div>
       <div className="text-center text-sm">
-        Volar para o {" "}
+        Voltar para o {" "}
         <Link href="/pt/" className="underline">
           Login
         </Link>
--- apps/web/app/[locale]/auth/recover-password/page.tsx
+++ apps/web/app/[locale]/auth/recover-password/page.tsx
@@ -1,11 +1,55 @@
+'use client'
+
+import { useState } from 'react'
+
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { Form, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form';
+import { z } from "zod"
+import { useToast } from "@/components/ui/use-toast"
+
 import Link from "next/link"
 
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
 import { AuthLayout } from "@/components/auth-layout"
 
+const FormSchema = z.object({
+  password: z.string({
+    required_error: "É necessário colocar a senha",
+  }).min(4, {
+    message: "Senha deve ter mais que 4 caracteres"
+  }),
+  confirmPassword: z.string({
+    required_error: "É necessário colocar confirmar a senha",
+  }).min(4, {
+    message: "Senha deve ter mais que 4 caracteres"
+  }),
+}).refine(({ confirmPassword, password }) => confirmPassword === password, {
+  message: 'As senhas não correspondem',
+})
+
+
 export default function RecoverPasswordPage() {
+  const [passwordError, setPasswordError] = useState("")
+
+  const { toast } = useToast()
+
+  const form = useForm<z.infer<typeof FormSchema>>({
+    resolver: zodResolver(FormSchema),
+  })
+
+  function onSubmit(data: z.infer<typeof FormSchema>) {
+    if (data.password !== data.confirmPassword) {
+      setPasswordError("As senhas não correspondem");
+      return;
+    }
+
+    toast({
+      title: "Cadastro realizado com sucesso!",
+    })
+  }
+
   return (
     <AuthLayout>
       <div className="grid gap-2 text-center">
@@ -15,21 +59,39 @@ export default function RecoverPasswordPage() {
         </p>
       </div>
       <div className="grid gap-4">
-        <div className="grid gap-2">
-          <div className="flex items-center">
-            <Label htmlFor="password">Senha</Label>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+          <div className="grid gap-2">
+            <FormField
+              control={form.control}
+              name="password"
+              render={({field}) => (
+                <FormItem>
+                  <FormLabel>Nova Senha</FormLabel>
+                  <Input id="password" type="password" {...field}/>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
           </div>
-          <Input id="password" type="password" required />
-        </div>
-        <div className="grid gap-2">
-          <div className="flex items-center">
-            <Label htmlFor="password">Repetir Senha</Label>
+          <div className="grid gap-2">
+            <FormField
+              control={form.control}
+              name="confirmPassword"
+              render={({field}) => (
+                <FormItem>
+                  <FormLabel>Confirmar Senha</FormLabel>
+                  <Input id="confirmPassword" type="password" {...field}/>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
           </div>
-          <Input id="password" type="password" required />
-        </div>
-        <Button type="submit" className="w-full">
-          Redefinir senha
-        </Button>
+            <Button type="submit" className="w-full">
+              Redefinir senha
+            </Button>
+          </form>
+        </Form>
       </div>
       <div className="mt-4 text-center text-sm">
         Já tem cadastro?{" "}
