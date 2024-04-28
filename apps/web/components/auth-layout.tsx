39c7a5f3a536b4ccb25c274b76c87cdda3b9c9c1
matheus1714 <matheussilvamartins1714@gmail.com>
2024-04-28 10:48:30 -0300

    fix(web): image cover

--- apps/web/components/auth-layout.tsx
+++ apps/web/components/auth-layout.tsx
@@ -7,13 +7,13 @@ export function AuthLayout({ children }: {
 }) {
   return (
     <div className="h-screen w-full lg:grid lg:min-h-[600px] lg:grid-cols-2 xl:min-h-[800px] flex items-center justify-center">
-      <div className="hidden bg-muted lg:block">
+      <div className="hidden bg-muted lg:block h-full">
         <Image
           src="/banner.jpeg"
           alt="Image"
           width="1920"
           height="1080"
-          className="h-screen w-full object-cover dark:brightness-[0.2] dark:grayscale"
+          className="h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
           priority
         />
       </div>
